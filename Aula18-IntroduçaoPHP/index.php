ab62afdfe682da5bc5a8d0834ebf88f4abbadd11
merlini-rk <merlinihenrique@gmail.com>
2025-05-08 10:15:55 -0300

    .
    .

---
+++ Aula18-IntroduçaoPHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Programa</title>
+</head>
+<body>
+    
+    <h1>
+        <?php
+            echo "Olá Mundo! <br>";
+        
+        ?>
+
+    </h1>
+
+</body>
+</html>
